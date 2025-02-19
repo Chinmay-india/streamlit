0b9d6927748dc1fb92c5ed21be0634265bd5ab27
lukasmasuch <lukas.masuch@gmail.com>
2025-02-19 20:06:38 +0100

    Fix license

--- frontend/lib/src/components/widgets/DataFrame/columns/cells/JsonCell.tsx
+++ frontend/lib/src/components/widgets/DataFrame/columns/cells/JsonCell.tsx
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2024)
+ * Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2025)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import * as React from "react"
 
 import JSON5 from "json5"
