a2eab7776bd6321303a00f1c26002e9df8ab4277
lukasmasuch <lukas.masuch@gmail.com>
2025-02-25 14:16:51 +0100

    Scroll into view

--- e2e_playwright/st_code_test.py
+++ e2e_playwright/st_code_test.py
@@ -56,6 +56,8 @@ def test_code_blocks_render_correctly(
     assert_snapshot(code_blocks.nth(6), name="st_code-diff_lang")
 
     # Test long lines draw as expected.
+    # The screenshot for long-no_wrap seems to be a bit flaky, scrolling
+    # it into view seems to help fix this (but not sure why).
     code_blocks.nth(11).scroll_into_view_if_needed()
     assert_snapshot(code_blocks.nth(11), name="st_code-long-no_wrap")
     assert_snapshot(code_blocks.nth(12), name="st_code-long-numbers-no_wrap")
