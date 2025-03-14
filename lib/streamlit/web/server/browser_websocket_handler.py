1557fb98adbb91d3650373fe119c6ceefadcdb50
Karen Javadyan <kajarenc@gmail.com>
2025-03-14 20:18:54 +0400

    Run tests with tornado await write_msg

--- lib/streamlit/web/server/browser_websocket_handler.py
+++ lib/streamlit/web/server/browser_websocket_handler.py
@@ -115,10 +115,10 @@ class BrowserWebSocketHandler(WebSocketHandler, SessionClient):
 
         return user_info
 
-    def write_forward_msg(self, msg: ForwardMsg) -> None:
+    async def write_forward_msg(self, msg: ForwardMsg) -> None:
         """Send a ForwardMsg to the browser."""
         try:
-            self.write_message(serialize_forward_msg(msg), binary=True)
+            await self.write_message(serialize_forward_msg(msg), binary=True)
         except tornado.websocket.WebSocketClosedError as e:
             raise SessionClientDisconnectedError from e
 
