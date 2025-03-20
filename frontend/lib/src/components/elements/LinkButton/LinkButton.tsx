08b25bff6b246df7e952d41849b11015364995af
lukasmasuch <lukas.masuch@gmail.com>
2025-03-20 11:21:47 +0100

    Cleanup

--- frontend/lib/src/components/elements/LinkButton/LinkButton.tsx
+++ frontend/lib/src/components/elements/LinkButton/LinkButton.tsx
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import React, { memo, MouseEvent, ReactElement } from "react"
+import React, { memo, ReactElement } from "react"
 
 import { LinkButton as LinkButtonProto } from "@streamlit/protobuf"
 
