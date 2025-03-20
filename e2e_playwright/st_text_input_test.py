687e50ce91c1dcb56c053fca6e70edc20613acf2
Maya Barnes <maya8617@gmail.com>
2025-03-20 16:50:35 -0700

    Add python tests & snapshots

--- e2e_playwright/st_text_input_test.py
+++ e2e_playwright/st_text_input_test.py
@@ -51,7 +51,9 @@ def test_text_input_widget_rendering(
 def test_text_input_has_correct_initial_values(app: Page):
     """Test that st.text_input has the correct initial values."""
     markdown_elements = app.get_by_test_id("stMarkdown")
-    # 1 st.write for each text input value (up until 14) + 1 st.write for "text input changed" + 1 st.write for "Rerun counter"
+    # 1 st.write for each text input value (up until 13)
+    # + 1 st.write for "text input changed"
+    # + 1 st.write for "Rerun counter"
     expect(markdown_elements).to_have_count(TEXT_INPUT_ELEMENTS - 1)
 
     expected = [
