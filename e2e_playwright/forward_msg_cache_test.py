7daa6a0732fb196f81b0543c4cba37ea4cc0abf2
lukasmasuch <lukas.masuch@gmail.com>
2025-03-25 13:03:27 +0100

    Update threshold

--- e2e_playwright/forward_msg_cache_test.py
+++ e2e_playwright/forward_msg_cache_test.py
@@ -96,9 +96,9 @@ def test_check_total_websocket_message_number_and_size(page: Page, app_port: int
 
     # ForwardMsg's
     TOTAL_WEBSOCKET_RECEIVED_SIZE_THRESHOLD_MB: Final = 55
-    # Number of websocket messages received (the check below allows some
-    # fluctuation)
-    EXPECTED_WEBSOCKET_MESSAGES_RECEIVED: Final = 2542
+    # Max number of websocket messages received. There can be a bit of
+    # fluctuation because of some optimization logic (e.g. composable messages)
+    WEBSOCKET_MESSAGES_RECEIVED_THRESHOLD: Final = 3000
 
     total_websocket_sent_size_bytes = 0
     total_websocket_received_size_bytes = 0
@@ -165,15 +165,10 @@ def test_check_total_websocket_message_number_and_size(page: Page, app_port: int
     _rerun_app(page, 10)
 
     # Assert that the total number of websocket messages received and sent is equal
-    assert (
-        abs(total_websocket_messages_received - EXPECTED_WEBSOCKET_MESSAGES_RECEIVED)
-        < 10  # We allow some fluctuation here since we have some optimization logic
-        # (composable messags) that might cause some variation in the number of
-        # websocket messages received.
-    ), (
+    assert total_websocket_messages_received < WEBSOCKET_MESSAGES_RECEIVED_THRESHOLD, (
         f"Total number of websocket messages received by the frontend "
-        f"{total_websocket_messages_received} but expected to receive "
-        f"{EXPECTED_WEBSOCKET_MESSAGES_RECEIVED}. In case this is expected, "
+        f"{total_websocket_messages_received} but expected to receive less than "
+        f"{WEBSOCKET_MESSAGES_RECEIVED_THRESHOLD}. In case this is expected, "
         "you can change the number in the test."
     )
     assert total_websocket_messages_sent == EXPECTED_WEBSOCKET_MESSAGES_SENT, (
