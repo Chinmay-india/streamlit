7842b0652e6561359dd680bf354531fc22aec462
Laura Wilby <laura.wilby@snowflake.com>
2025-04-19 16:47:13 +0200

    try same settings as checkbox

--- e2e_playwright/st_radio_test.py
+++ e2e_playwright/st_radio_test.py
@@ -83,8 +83,9 @@ def test_set_value_correctly_when_click(app: Page):
     """Test that st.radio returns the correct values when the selection is changed."""
     for index, element in enumerate(app.get_by_test_id("stRadio").all()):
         if index not in [2, 3]:  # skip disabled and no-options widget
+            element.scroll_into_view_if_needed()
             radio_option = element.locator('label[data-baseweb="radio"]').nth(1)
-            radio_option.click()
+            radio_option.click(delay=50, force=True)
             wait_for_app_run(app)
 
     expected = [
