b9364463136a0edffd35e188533ffbff54606cb1
Laura Wilby <laura.wilby@snowflake.com>
2025-04-21 23:55:19 +0200

    TypeAlias import

--- lib/streamlit/elements/lib/layout_utils.py
+++ lib/streamlit/elements/lib/layout_utils.py
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Literal, TypeAlias, Union
+from typing import Literal, Union
+
+from typing_extensions import TypeAlias
 
 from streamlit.errors import StreamlitInvalidWidthError
 
