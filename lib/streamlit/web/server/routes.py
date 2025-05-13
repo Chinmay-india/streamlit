d21303e789e04ad5e4c21a62097ff2abd0855be0
lukasmasuch <lukas.masuch@gmail.com>
2025-05-13 13:06:06 +0200

    Update

--- lib/streamlit/web/server/routes.py
+++ lib/streamlit/web/server/routes.py
@@ -212,7 +212,7 @@ _DEFAULT_ALLOWED_MESSAGE_ORIGINS = [
 
 
 class HostConfigHandler(_SpecialRequestHandler):
-    def initialize(self):
+    def initialize(self) -> None:
         # Make a copy of the allowedOrigins list, since we might modify it later:
         self._allowed_origins = _DEFAULT_ALLOWED_MESSAGE_ORIGINS.copy()
 
