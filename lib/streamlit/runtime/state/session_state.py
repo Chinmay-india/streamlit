d530ee1630773470f4b38951d1d8ce6c31c0640e
lukasmasuch <lukas.masuch@gmail.com>
2025-05-15 12:30:58 +0200

    Add more typings

--- lib/streamlit/runtime/state/session_state.py
+++ lib/streamlit/runtime/state/session_state.py
@@ -325,7 +325,7 @@ class KeyIdMapper:
         self._key_id_mapping.update(other._key_id_mapping)
         self._id_key_mapping.update(other._id_key_mapping)
 
-    def clear(self):
+    def clear(self) -> None:
         self._key_id_mapping.clear()
         self._id_key_mapping.clear()
 
