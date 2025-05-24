3f858c3e4b7eb08f8ca2fa0af5e9348b1d171ab4
xtliu97 <mielionsk@outlook.com>
2025-05-24 10:42:39 +0800

    [fix] add  prop support to MaterialFontIcon

--- frontend/lib/src/components/shared/Icon/DynamicIcon.tsx
+++ frontend/lib/src/components/shared/Icon/DynamicIcon.tsx
@@ -22,6 +22,7 @@ import { IconSize, EmotionTheme, getMarkdownTextColors } from "~lib/theme"
 import { EmojiIcon } from "./Icon"
 import MaterialFontIcon from "./Material/MaterialFontIcon"
 import { StyledDynamicIcon, StyledImageIcon } from "./styled-components"
+import { hasLightBackgroundColor } from "~lib/theme"
 
 interface IconPackEntry {
   pack: string
@@ -140,6 +141,7 @@ export const DynamicIcon = (props: DynamicIconProps): React.ReactElement => {
     props.iconValue
   )
   const theme: EmotionTheme = useTheme()
+  const themeMode = hasLightBackgroundColor(theme) ? "light" : "dark"
   const colorMapping = createColorMapping(theme)
   const style = parsedColor ? colorMapping.get(parsedColor) : {}
 
@@ -155,7 +157,7 @@ export const DynamicIcon = (props: DynamicIconProps): React.ReactElement => {
           <EmojiIcon {...mergedProps}>&nbsp;</EmojiIcon>
         </StyledDynamicIcon>
       }
-      key={mergedProps.iconValue}
+      key={`${mergedProps.iconValue}-${themeMode}`}
     >
       <DynamicIconDispatcher {...mergedProps} />
     </Suspense>
